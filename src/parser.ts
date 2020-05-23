aafc53af6684016bb1a9ff50ace2b6a1ae9b08a6
SIY1121 <sota.0512.0213@gmail.com>
2020-05-23 23:20:53 +0900

    initial commit

---
+++ src/parser.ts
@@ -0,0 +1,27 @@
+import { SfzRegion } from "./types";
+
+export function parseSFZ(sfzText: string): SfzRegion[] {
+  sfzText = sfzText.replace(/\/\/.*$/gm, "");
+  return matchAll(sfzText, /<(.*?)>\s([\s\S]*?)((?=<)|\Z)/gm).map((res) => {
+    const kvs = matchAll(res[2], /(.*?)=(.*?)(\s|$)/gm);
+    const prop: any = {};
+    kvs.forEach((kv) => {
+      prop[kv[1].replace(/\s/gm, "")] = isNaN(parseInt(kv[2]))
+        ? kv[2]
+        : Number(kv[2]);
+    });
+    return {
+      type: res[1],
+      property: prop,
+    };
+  });
+}
+
+function matchAll(str: string, regexp: RegExp) {
+  let match: RegExpExecArray | null = null;
+  const res = [];
+  while ((match = regexp.exec(str)) !== null) {
+    res.push(match);
+  }
+  return res;
+}

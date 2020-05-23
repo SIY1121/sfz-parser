ef8fdb92c7a787a0403ceafc26939342614b6202
SIY1121 <sota.0512.0213@gmail.com>
2020-05-24 01:44:44 +0900

    add: note number transform

--- src/parser.ts
+++ src/parser.ts
@@ -9,8 +9,10 @@ export default function (sfzText: string): SfzRegion[] {
       prop[kv[1].replace(/\s/gm, "")] = /^\d*$/g.test(kv[2])
         ? Number(kv[2])
         : kv[2];
+      if (/^[a-g]#?\d$/.test(kv[2])) prop[kv[1]] = name2num(kv[2]);
     });
     if (prop.sample) prop.sample = prop.sample.replace(/\\/g, "/");
+
     return {
       type: res[1],
       property: prop,
@@ -25,4 +27,31 @@ function matchAll(str: string, regexp: RegExp) {
     res.push(match);
   }
   return res;
+}
+
+function name2num(name: string): number {
+  const tmp = name.match(/^([a-g])(#?)(\d)$/);
+  if (!tmp) return -1;
+  const d = tmp[1];
+  const s = tmp[2];
+  const o = Number(tmp[3]);
+  let res = (o + 1) * 12 + (s === "#" ? 1 : 0);
+  switch (d) {
+    case "c":
+      return res;
+    case "d":
+      return res + 2;
+    case "e":
+      return res + 4;
+    case "f":
+      return res + 5;
+    case "g":
+      return res + 7;
+    case "a":
+      return res + 9;
+    case "b":
+      return res + 11;
+    default:
+      return -1;
+  }
 }
